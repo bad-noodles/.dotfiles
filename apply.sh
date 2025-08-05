95952eec762b3905fa6cc995e858cc430270c7ae
BadNoodles <149381511+bad-noodles@users.noreply.github.com>
2025-08-05 16:21:49 -0300

    Update apply.sh

--- apply.sh
+++ apply.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
+# Install Homebrew and deps
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install git stow
+
+# Download dotfiles
+cd ~
+git clone git@github.com:bad-noodles/.dotfiles.git
+
+# Backup existing config and apply
+mv .zprofile .zprofile_bkp
+cd ./dotfiles
 stow --target=$HOME .
