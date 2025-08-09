e31c90646b02ba020303b1fbe2d4a1aa60a2a9fd
Rafa <pinheiro.rbp@gmail.com>
2025-08-09 01:31:34 -0300

    homebrew fix

--- apply.zsh
+++ apply.zsh
@@ -2,7 +2,7 @@
 
 echo "===   Installing Homebrew   ==="
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-/opt/homebrew/bin/brew shellenv
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 echo "===   Installing Brew Dependencies   ==="
 brew bundle
